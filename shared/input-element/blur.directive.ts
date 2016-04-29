77086e1c6713df323bdeb565b20970b70e70e02e
Jeanie Keonine <jeaniekeonine@gmail.com>
2016-04-28 23:17:01 -0500

    upgrade from tsd to typings

--- shared/input-element/blur.directive.ts
+++ shared/input-element/blur.directive.ts
@@ -1,4 +1,4 @@
-/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/browser.d.ts" />
 /// <reference path="../angular/constants.ts" />
 
 var ngConstants = shared.angular.constants;
